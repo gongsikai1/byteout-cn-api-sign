cdb02f19f85cc125e034f9997252cf4d0e98f59f
gongsikai <gongsikai@189.cn>
2025-08-02 16:51:00 +0800

    add: support main send

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ console.log('t', t)
 // 新增定时请求函数
 // const randomNumber = 2000
 const randomNumber = Math.floor(Math.random() * 3001) + 2000;
-setInterval(() => {
+setInterval(async () => {
     // 每次请求生成新的时间戳和随机数
     const newTimestamp = Date.now();
     const newNonce = Math.random().toString(36).substring(2, 10);
@@ -57,16 +57,54 @@ setInterval(() => {
     t.headers["X-Signature"] = Jp(t.params, t.data, newTimestamp, newNonce);
 
     // 发送GET请求
-    axios.get('https://www.byteout.cn/api/auth/captcha', {
+    // axios.get('https://www.byteout.cn/api/auth/captcha', {
+    //     headers: {
+    //         ...t.headers,
+    //         hello: Math.random().toString(36).substring(2, 10)
+    //     }
+    // })
+    // .then(response => {
+    //     console.log(`[${new Date().toLocaleTimeString()}] 请求成功:`, response.status);
+    // })
+    // .catch(error => {
+    //     console.error(`[${new Date().toLocaleTimeString()}] 请求失败:`, error.message);
+    // });
+
+    axios.get('https://www.byteout.cn/api/auth/sendMailCode/fl9420@qq.com/PASSWORD-RESET', {
         headers: {
             ...t.headers,
             hello: Math.random().toString(36).substring(2, 10)
         }
     })
     .then(response => {
-        console.log(`[${new Date().toLocaleTimeString()}] 请求成功:`, response.status);
+        console.log(`[${new Date().toLocaleTimeString()}] 请求成功:`, response.status, response.data);
     })
     .catch(error => {
         console.error(`[${new Date().toLocaleTimeString()}] 请求失败:`, error.message);
     });
+
+    // const res = await fetch("https://www.byteout.cn/api/auth/sendMailCode/fl9420@qq.com/PASSWORD-RESET", {
+    // "headers": {
+    //     "accept": "application/json, text/plain, */*",
+    //     "accept-language": "zh-CN,zh;q=0.9",
+    //     "cache-control": "no-cache",
+    //     "pragma": "no-cache",
+    //     "priority": "u=1, i",
+    //     "sec-ch-ua": "\"Google Chrome\";v=\"135\", \"Not-A.Brand\";v=\"8\", \"Chromium\";v=\"135\"",
+    //     "sec-ch-ua-mobile": "?0",
+    //     "sec-ch-ua-platform": "\"Windows\"",
+    //     "sec-fetch-dest": "empty",
+    //     "sec-fetch-mode": "cors",
+    //     "sec-fetch-site": "same-origin",
+    //     "x-nonce": `${newNonce}`,
+    //     "x-signature": `${t.headers["X-Signature"]}`,
+    //     "x-timestamp": `${newTimestamp}`,
+    //     "Referer": "https://www.byteout.cn/password-reset",
+    //     "Referrer-Policy": "strict-origin-when-cross-origin",
+    //     // ...t.headers,
+    // },
+    // "body": null,
+    // "method": "GET"
+    // });
+    // console.log('res', res.body)
 }, randomNumber); // 随机时间间隔执行
