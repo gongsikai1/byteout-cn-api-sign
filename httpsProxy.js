5b1ca3d3ecbc16c1d4b307f5d5060e4010d87269
gongsikai <gongsikai@189.cn>
2025-08-14 08:57:10 +0800

    mod: change url

--- httpsProxy.js
+++ httpsProxy.js
@@ -5,18 +5,19 @@ const getHTTPSProxy = async () => {
     let HTTPSProxy = null;
     const locations = {
         putian: 'http://v2.api.juliangip.com/company/postpay/getips?auto_white=1&city=莆田&num=1&pt=1&result_type=json&trade_no=6792146775149790&sign=f271c25f90c87c16381ad1988bb9ba41',
-        random: 'http://v2.api.juliangip.com/company/postpay/getips?auto_white=1&num=1&pt=1&result_type=json&trade_no=6792146775149790&sign=3ebb4cd5cf9b5605f8e79fea844297c1'
+        random: 'http://v2.api.juliangip.com/company/postpay/getips?auto_white=1&num=1&pt=1&result_type=json&trade_no=6792146775149790&sign=3ebb4cd5cf9b5605f8e79fea844297c1',
+        chongqing: 'http://v2.api.juliangip.com/company/postpay/getips?auto_white=1&city=重庆&num=1&pt=1&result_type=json&trade_no=6792146775149790&sign=da6f160cb58a3992297567cbcd1c7abc',
     }
     let res = null;
     let proxyAgent = null;
     try {
-        res = await axios.get(locations.random)
+        res = await axios.get(locations.chongqing)
         console.log('res', res)
         // console.log('r', res.data.data.proxy_list[0])
         // res = {"code":200,"msg":"成功","data":{"count":1,"filter_count":1,"surplus_quantity":0,"proxy_list":["110.53.250.157:25538"]}}
         const [ip, port] = (res || {})?.data?.data?.proxy_list[0]?.split(':')
         const proxy_addr = `http://${ip}:${port}`;
-        // const proxy_addr = 'http://42.48.26.100:16298';
+        // const proxy_addr = '110.53.165.98:31592';
         console.log('proxy_addr', proxy_addr)
         proxyAgent = await new HttpsProxyAgent(proxy_addr);
         // const r = await proxyAgent
